8dcf70bf1bf485ee43e290671ed0663ced7cb102
GustavoCaso <gustavocaso@gmail.com>
2017-06-26 22:58:09 +0200

    Initial commit

---
+++ lib/til.ex
@@ -0,0 +1,18 @@
+defmodule Til do
+  @moduledoc """
+  Documentation for Til.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Til.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/til_test.exs
@@ -0,0 +1,8 @@
+defmodule TilTest do
+  use ExUnit.Case
+  doctest Til
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
